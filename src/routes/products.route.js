38cf1de6b546562c9ee48bf888a3d14b96a64188
Loly Maluf <lolymaluf@gmail.com>
2022-09-23 21:41:46 -0300

    Adiciona Requisito 04

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
+const getValidation = require('../middlewares/nameValidation');
 
 const router = express.Router();
 
 router.get('/', productsController.getAllProducts);
-
 router.get('/:id', productsController.getProductsById);
-
 router.post('/', productsController.registerProducts);
+router.post('/', getValidation.nameValidation, productsController.registerProducts);
 
 module.exports = router;
