cae98db33054f4b7fb447b25d5f5579efd45e309
Loly Maluf <lolymaluf@gmail.com>
2022-09-26 19:34:55 -0300

    12

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -28,13 +28,6 @@ const updateProduct = async (req, res) => {
   if (!response) return res.status(404).json({ message: 'Product not found' });
   return res.status(200).json(response);
 };
-/* 
-const deleteThisProduct = async (req, res) => {
-  const { id } = req.params;
-  const { type, message } = await productService.deleteThisProduct(id);
-  if (type) return res.status(404).json({ message });
-  res.status(204);
-}; */
 
 const deleteThisProduct = async (req, res) => {
   const { id } = req.params;
