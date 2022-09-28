8aa9fbf9c0ad6e39812d3f50315d58557a417d15
Loly Maluf <lolymaluf@gmail.com>
2022-09-27 22:56:01 -0300

    Adiciona testes Camada Product Service

---
+++ tests/unit/services/products.services.test.js
@@ -0,0 +1,74 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+
+const productService = require("../../../src/services/products.services");
+const productModel = require("../../../src/models/products.model");
+
+const {
+  productsMock,
+  product,
+  productId,
+  createProduct,
+} = require("../models/mocks/product.mock");
+
+
+describe("Testa Products Service", () => {
+  afterEach(async () => {
+    sinon.restore();
+  });
+
+  it("Testa se função getAllProducts da Service", async () => {
+    sinon.stub(productModel, "getAllProducts").resolves(productsMock.products);
+    const products = await productService.getAllProducts();
+    expect(products).to.deep.equal(productsMock.products);
+  });
+
+  it("Testa a função getProductById da Service", async () => {
+    sinon.stub(productModel, "getProductById").resolves(productId);
+    const result = await productService.getProductById(1);
+    expect(result).to.equal(productId);
+  });
+
+  it("Testa função registerProducts da Service", async () => {
+    sinon.stub(productModel, "registerProducts").resolves({ insertId: 4 });
+    const results = await productService.registerProducts(createProduct.name);
+
+    expect(results).to.deep.equal({ id: 4, name: createProduct.name });
+  });
+
+  it("Testa função updateProduct da Service", async () => {
+    sinon.stub(productModel, "updateProduct").resolves(product);
+    const results = await productService.updateProduct(1, "Martelo de Thor");
+
+    expect(results).to.deep.equal(product);
+  });
+
+    it("Testa updateProduct, se não existir", async () => {
+      sinon.stub(productModel, "getProductById").resolves();
+      const results = await productService.updateProduct(1, "Martelo de Thor");
+      expect(results).to.deep.equal(null);
+    });
+
+  it("Testa função deleteThisProduct da Service", async () => {
+    sinon.stub(productModel, "deleteThisProduct").resolves(product);
+    const results = await productService.deleteThisProduct(1);
+
+    expect(results).to.deep.equal(product);
+  });
+
+ it("Testa deleteThisProduct, se não existir", async () => {
+   sinon.stub(productModel, "getProductById").resolves();
+    const results = await productService.deleteThisProduct(
+      1,
+      "Martelo de Thor"
+    );
+    expect(results).to.deep.equal(null);
+ });
+
+  it("Testa função searchProduct da Service", async () => {
+    sinon.stub(productModel, "searchProduct").resolves([product]);
+    const results = await productService.searchProduct("Martelo");
+
+    expect(results).to.deep.equal([product]);
+  });
+});
